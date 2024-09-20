b08e1f2d9d5b5d8ec1ffa25be67e04681139d87e
Nishant Shrestha <49269819+sNishant011@users.noreply.github.com>
2024-09-20 07:44:25 +0545

    Error handling for storage access

--- packages/core/src/storage/utils.ts
+++ packages/core/src/storage/utils.ts
@@ -7,16 +7,38 @@ import { InMemoryStorage } from './InMemoryStorage';
  * @internal
  * @returns Either a reference to window.localStorage or an in-memory storage as fallback
  */
-export const getLocalStorageWithFallback = (): Storage =>
-	typeof window !== 'undefined' && window.localStorage
-		? window.localStorage
-		: new InMemoryStorage();
+export const getLocalStorageWithFallback = (): Storage => {
+  try {
+    // Attempt to use localStorage directly
+    if (typeof window !== 'undefined' && window.localStorage) {
+      // Test if localStorage is accessible
+      return window.localStorage;
+    }
+  } catch (e) {
+    // Handle any errors related to localStorage access
+    console.error('LocalStorage access failed:', e);
+  }
+
+  // Return in-memory storage as a fallback if localStorage is not accessible
+  return new InMemoryStorage();
+};
 
 /**
  * @internal
  * @returns Either a reference to window.sessionStorage or an in-memory storage as fallback
  */
-export const getSessionStorageWithFallback = (): Storage =>
-	typeof window !== 'undefined' && window.sessionStorage
-		? window.sessionStorage
-		: new InMemoryStorage();
+export const getSessionStorageWithFallback = (): Storage => {
+  try {
+    // Attempt to use localStorage directly
+    if (typeof window !== 'undefined' && window.sessionStorage) {
+      // Test if localStorage is accessible
+      return window.sessionStorage;
+    }
+  } catch (e) {
+    // Handle any errors related to localStorage access
+    console.error('LocalStorage access failed:', e);
+  }
+
+  // Return in-memory storage as a fallback if localStorage is not accessible
+  return new InMemoryStorage();
+};
